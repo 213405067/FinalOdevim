6252d23b14f25e8f120f0f71d04213d044cd726b
213405067 <rumeysafeyzaugrasskan@gmail.com>
2023-05-28 02:08:48 +0300

    final ödevim

---
+++ app/src/main/java/com/lesson/finalprojeodevi/veritabani.java
@@ -0,0 +1,11 @@
+package com.lesson.finalprojeodevi;
+
+
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Not.class}, version =1)
+public abstract class veritabani extends RoomDatabase {
+    public abstract dao dao();
+}
